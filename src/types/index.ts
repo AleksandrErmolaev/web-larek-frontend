edc9cbc982efa2a5a6ee63bf837db17c31efbc4c
Александр <shura.yermolayev.05@mail.ru>
2025-05-20 15:42:45 +0300

    weblarek1

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+export type TOrderInvoice = Omit<IOrder, 'items'> & {
+	items: string[];
+	total: number;
+};
+
+export type TOrderPayment = 'cash' | 'card';
+
+export type TOrderStep = 'shipment' | 'contacts';
+
+export interface IOrder {
+	items: IProduct[];
+	payment: TOrderPayment;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
